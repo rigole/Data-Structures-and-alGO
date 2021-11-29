46de4bd54511b0d7221f3d14af065a331b64fd4d
rigole <foplacide@gmail.com>
2021-11-29 10:24:35 +0100

    adding and starting the sololearn courses with Go

---
+++ solo.go
@@ -0,0 +1,57 @@
+
+package main
+
+import (
+
+	"fmt"
+)
+
+func main() {
+
+	//var i int = 97
+	//x, y := 10, 5
+
+	/*var x int = 42
+	var y float32 = 1.37
+	var name string = "Plass"
+	var online bool = true
+
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(name)
+	fmt.Println(online)*/
+
+	// Arithmetics
+
+	x := 42
+	y := 8
+
+	res := x + y 
+	fmt.Println(res)
+
+
+	res2 := x - y 
+
+	fmt.Println(res2)
+
+
+	res3 := x * y 
+
+	fmt.Println(res3)
+
+
+	res4 := x / y 
+	fmt.Println(res4)
+
+
+	res5 := x%y 
+
+	fmt.Println(res5)
+
+
+
+
+}
+
+
