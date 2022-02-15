225624c159a823a39dcfa33986704e8c6a681a45
rigole <foplacide@gmail.com>
2022-02-15 07:49:47 +0100

    creating the queue file to implement a queue

---
+++ Stack and Queues/queue.js
@@ -0,0 +1,24 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    
+    }
+    peek(){
+
+    }
+
+    dequeue(){
+
+    }
+}
+
+const myQueue = new Queue(); 
