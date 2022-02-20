5477762c925bf85e5a994f01c7179e5501633124
rigole <foplacide@gmail.com>
2022-02-20 08:24:52 +0100

    queue class

--- Stack and Queues/queue.js
+++ Stack and Queues/queue.js
@@ -13,11 +13,33 @@ class Queue {
     
     }
     peek(){
+        return this.first;
+    }
+    enqueue(value){
+        const newNode = new Node(value)
+        if(this.length === 0){
 
+            this.first = newNode;
+            this.last =  newNode;
+
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
     }
 
     dequeue(){
-
+        if(!this.first){
+            return null;
+        }
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.length--;
+        return this;
     }
 }
 
